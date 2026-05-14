7613635946bcdf348e58ff0f1a3d922857593a32
Anv0rax <anvorax@gmail.com>
2026-05-14 16:31:01 +0200

    Can register if the api isn't available

--- ClickCollect_Antoine_Nolan_2026/Models/Adress.cs
+++ ClickCollect_Antoine_Nolan_2026/Models/Adress.cs
@@ -116,7 +116,7 @@ namespace ClickCollect_Antoine_Nolan_2026.Models
 
             if (results.GetArrayLength() == 0)
             {
-                throw new Exception($"Invalid adress : {address}");
+                throw new ArgumentException($"Invalid adress : {address}");
             }
 
             var res = results[0];
