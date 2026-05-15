e8db191bead71f5e29b48d84f6c2d5dc8a0d90f1
Anv0rax <anvorax@gmail.com>
2026-05-15 22:48:45 +0200

    regex correction

--- ClickCollect_Antoine_Nolan_2026/Models/User.cs
+++ ClickCollect_Antoine_Nolan_2026/Models/User.cs
@@ -19,26 +19,21 @@ namespace ClickCollect_Antoine_Nolan_2026.Models
         [Display(Name = "Username")]
         [Required(ErrorMessage = "Username is required.")]
         [StringLength(25, MinimumLength = 4, ErrorMessage = "Username must be between 4 and 25 characters.")]
-        [RegularExpression(@"^[a-zA-Z0-9._-]+$", ErrorMessage = "Username contains invalid characters (only letters, numbers, dots, underscores and hyphens allowed).")]
+        [RegularExpression(@"^[a-zA-Z0-9._-]+$", ErrorMessage = "Username contains invalid characters.")]
         public string Username { get; set; } = string.Empty;
 
         [Display(Name = "Password")]
         [Required(ErrorMessage = "Password is required.")]
-        [DataType(DataType.Password)] // We are precising that the string will be a password, which will be helpful for the UI
-        [StringLength(100, MinimumLength = 12, ErrorMessage = "Password must be at least 12 characters long.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 12, ErrorMessage = "Must be at least 12 characters long.")]
 
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$", ErrorMessage = "Password too weak: requires 1 uppercase, 1 lowercase, and one number")]
-        // The regex of password: We want at least an uppercase, a lowercase, a number.
-        // Here, the (?=.) means that we need in the password at least the input in front of the point.
-        // For example, (?=.*\d) means that we need at least a digit.
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{12,40}$", ErrorMessage = "Requires at least 12 characters with 1 uppercase, 1 lowercase, and one number")]
         public string Password { get; set; } = string.Empty;
 
         [Display(Name = "Last name")]
         [Required(ErrorMessage = "The name is required to continue.")]
         [RegularExpression(@"^[^\d]+$", ErrorMessage = "The name can't contain any digits. Please delete them.")]
-        // The characters ^ means we are not authorizing what's going to be next. \d means any digit, so we don't accept any digits in the name
         [StringLength(50, MinimumLength = 2, ErrorMessage = "The name must have between 2 and 50 characters.")]
-        // The name must be between 2 (minimal length) and 50 (maximal length) characters.
         public string LastName
         {
             get { return lastName; }
@@ -56,7 +51,6 @@ namespace ClickCollect_Antoine_Nolan_2026.Models
         }
 
 
-        // Constructors with no argument and with arguments.
         public User()
         {
 
