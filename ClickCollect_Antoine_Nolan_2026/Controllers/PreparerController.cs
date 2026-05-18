e9bf293ad3b250e68809caf00a36443a8eabd359
Nolan <nolanzerrkam@gmail.com>
2026-05-19 01:04:41 +0200

    Details and Prepare view done : order status changing to ready.

--- ClickCollect_Antoine_Nolan_2026/Controllers/PreparerController.cs
+++ ClickCollect_Antoine_Nolan_2026/Controllers/PreparerController.cs
@@ -36,5 +36,72 @@ namespace ClickCollect_Antoine_Nolan_2026.Controllers
                 return View(new List<Order>());
             }
         }
+        // This will display the details of an spefiic order.
+        public async Task<IActionResult> Details(int id)
+        {
+            try
+            {
+                Order? order = await _orderDAL.GetOrderDetailsAsync(id);
+
+                if(order == null)
+                {
+                    ViewData["Error"] = "This order could not be found infortunealy.";
+                    return RedirectToAction("Index");
+                }
+
+                return View(order);
+            }
+            catch(Exception ex)
+            {
+                ViewData["Error"] = "An error has occured : " + ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        // To display the form to start preparing the order
+        public async Task<IActionResult> Prepare(int id)
+        {
+            try
+            {
+                Order? order = await _orderDAL.GetOrderDetailsAsync(id);
+
+                if (order == null)
+                {
+                    ViewData["Error"] = "This order could not be found.";
+                    return RedirectToAction("Index");
+                }
+
+                return View(order);
+            }
+            catch (Exception ex)
+            {
+                ViewData["Error"] = "An error has occured : " + ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        // Processes the preparation form and updates the order status
+        [HttpPost]
+        public async Task<IActionResult> Prepare(int id, int numberOfBoxUsed)
+        {
+            try
+            {
+                if (numberOfBoxUsed < 1)
+                {
+                    ViewData["Error"] = "You must use at least 1 box.";
+                    Order? orderError = await _orderDAL.GetOrderDetailsAsync(id);
+                    return View(orderError);
+                }
+
+                await _orderDAL.UpdateOrderStatusAsync(id, OrderStatusEnum.Ready, numberOfBoxUsed);
+                TempData["Success"] = "The order has been marked as ready.";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["Error"] = "An error has occured : " + ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
